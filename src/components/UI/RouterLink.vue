59631c7b1609ae570ae13d0b7a1580e13c4d6d6f
Kilian <dankestmirror@hotmail.es>
2025-12-02 10:38:38 +0000

    componet perso router

---
+++ src/components/UI/RouterLink.vue
@@ -0,0 +1,15 @@
+<script setup>
+    import { RouterLink } from 'vue-router';
+
+    defineProps({
+        to: {
+            type: String,
+        }
+    })
+</script>
+<template>
+    <RouterLink :to="{name: to}"
+    class="block md:inline-block rounded bg-blue-600 py-2 px-3">
+        <slot></slot>
+    </RouterLink>
+</template>
